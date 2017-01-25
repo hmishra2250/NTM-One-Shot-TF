970b49268f9df6d4e4b48853d84730e2b22dc7a5
Himadri Mishra <hmishra2250@gmail.com>
2017-01-25 17:40:41 +0530

    Initial commit for Omniglot testing

--- MANN/Omniglot.py
+++ MANN/Omniglot.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import numpy as np
-import pytest
+import time
 
 from .Model import memory_augmented_neural_network
+from .Utils.Generator import OmniglotGenerator
+from .Utils.Metrics import accuracy_instance
 
+def omniglot():
+    return
